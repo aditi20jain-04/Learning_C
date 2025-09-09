39dac96b96836cd5a2d462cd6b79031282c42c08
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-09-09 22:04:10 +0530

    Add files via upload

---
+++ Day19-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, lcm;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    for (lcm = (a > b ? a : b); ; lcm++) {
+        if (lcm % a == 0 && lcm % b == 0) {
+            printf("LCM = %d\n", lcm);
+            break;
+        }
+    }
+    return 0;
+}
