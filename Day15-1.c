fc60a72dc81813ce2896fb2c47e371bba3497b9f
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-09-11 20:18:49 +0530

    Add files via upload

---
+++ Day15-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+    int i,n,f=1;
+    printf("Enter the number: ");
+    scanf("%d" ,&n);
+    for (i=1; i<=n; i++)
+    {
+    f= f*i;
+    }
+    printf("Factorial is: %d" ,f);
+    return 0;
+}
