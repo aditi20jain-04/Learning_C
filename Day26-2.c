5d04d9924e5e166839599539257f2946832a7582
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-09-16 11:36:06 +0530

    Add files via upload

---
+++ Day26-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        if(i%2!=0)
+        {
+        for(j=1; j<=i; j++)
+        {
+            printf("*\n");
+        }
+        }
+        printf("\n");
+    }
+    for(i=n-1; i>=1; i--)
+    {
+        if(i%2!=0)
+        {
+        for(j=1; j<=i; j++)
+        {
+            printf("*\n");
+        }
+        }
+        printf("\n");
+    }
+    return 0;
+}
