989caddb5a6e741abd0336196a48e399c857e7f6
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-09-20 20:14:04 +0530

    Add files via upload

---
+++ Day30-2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, pos = 0, neg = 0, zero = 0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if(arr[i] > 0)
+            pos++;
+        else if(arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive = %d\n", pos);
+    printf("Negative = %d\n", neg);
+    printf("Zero     = %d\n", zero);
+
+    return 0;
+}
