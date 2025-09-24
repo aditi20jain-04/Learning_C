951517351ca237372da96333afe3a74ad5853fc9
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-09-24 20:41:57 +0530

    Add files via upload

---
+++ Day35-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Second largest = %d\n", arr[n - 2]);
+    return 0;
+}
