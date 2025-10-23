2d39fd62722fec6fba2f178b38db82e72120c003
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-10-23 12:14:26 +0530

    Add files via upload

---
+++ Day61.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, j;
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    printf("Enter k: ");
+    scanf("%d", &k);
+    
+    for(i = 0; i <= n - k; i++) {
+        int found = 0;
+        for(j = i; j < i + k; j++) {
+            if(arr[j] < 0) {
+                printf("%d ", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if(!found) printf("0 ");
+    }
+    
+    return 0;
+}
