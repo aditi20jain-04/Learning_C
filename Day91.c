5fa8b750085a1f66f2eaa4313a75448cf7c1c9b8
Aditi Jain <aditi.sparkle.20@gmail.com>
2025-11-21 18:45:30 +0530

    Add files via upload

---
+++ Day91.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s[5];
+
+    for(int i = 0; i < 5; i++) {
+        printf("Enter name of student %d: ", i+1);
+        scanf("%s", s[i].name);
+
+        printf("Enter roll number: ");
+        scanf("%d", &s[i].roll_no);
+
+        printf("Enter marks: ");
+        scanf("%f", &s[i].marks);
+
+        printf("\n");
+    }
+
+    printf("----- Student Details -----\n");
+    for(int i = 0; i < 5; i++) {
+        printf("Student %d\n", i+1);
+        printf("Name: %s\n", s[i].name);
+        printf("Roll No: %d\n", s[i].roll_no);
+        printf("Marks: %.2f\n\n", s[i].marks);
+    }
+
+    return 0;
+}
